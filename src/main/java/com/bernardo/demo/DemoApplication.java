255f5226c9eed638c7e7df22881201d60cd08a64
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2021-01-20 17:06:26 -0600

    return an object of student

--- src/main/java/com/bernardo/demo/DemoApplication.java
+++ src/main/java/com/bernardo/demo/DemoApplication.java
@@ -1,10 +1,13 @@
 package com.bernardo.demo;
 
+import com.bernardo.demo.student.StudentModel;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
+import java.time.Month;
 import java.util.List;
 
 @SpringBootApplication
@@ -16,7 +19,15 @@ public class DemoApplication {
 	}
 
 	@GetMapping
-	public List<String> hello() {
-		return List.of("Hello World");
+	public List<StudentModel> hello() {
+		return List.of(
+				new StudentModel(
+						1L,
+						"Bernie",
+						21,
+						LocalDate.of(2000, Month.APRIL, 5),
+						"example@example.com"
+				)
+		);
 	}
 }
